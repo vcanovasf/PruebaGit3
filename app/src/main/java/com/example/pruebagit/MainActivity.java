2e9fde80fed19ed7f6555aa236d96cf9cb3b7f15
Vicente Cánovas <vcanovas@tusestudios.com>
2020-04-22 17:11:41 +0200

    Sout

--- app/src/main/java/com/example/pruebagit/MainActivity.java
+++ app/src/main/java/com/example/pruebagit/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Esto es un comentario de prueba
+        System.out.println("Bla");
     }
 }
